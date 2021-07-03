03fe9602ff3e1e6192bf881d5716e2f351fa1510
Ricky <phangr28@gmail.com>
2021-07-03 16:31:10 +0700

    login regist beres

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,20 +4,31 @@
 <div class="content-login">
     <div class="logo-login" data-aos="zoom-in-right" data-aos-duration="1000" data-aos-easing="ease-in-out">
         <a href="#">
-            <img src="/storage/assets/logo1.png" alt="" width="100%">
+            <img src="/storage/assets/logo1.png" alt="" width="250px">
+            <p>Your Safety is our Priority</p>
         </a>
     </div>
-    <div class="content-wraper-login" data-aos="zoom-in-left" data-aos-duration="1000" data-aos-easing="ease-in-out">
-        <form method="GET">
+    <div class="content-wraper-login card border-0 shadow mt-3 " data-aos="zoom-in-left">
+        <div class="card-body my-3">
+            <form method="GET">
             <div class="login-form">
-                <label for="username"><b>Email :</b></label>
-                <input type="Email" name="email" id="email" placeholder="Email">
-                <label for="password"><b>Password :</b></label>
-                <input type="password" name="password" id="password" placeholder="Password">
+                <div class="wraping-form">
+                    <label for="username"><b>Email :</b></label>
+                    <p>
+                        <input type="Email" name="email" id="email" placeholder="Email">
+                    </p>
+                    <label for="password"><b>Password :</b></label>
+                    <p>
+                        <input type="password" name="password" id="password" placeholder="Password">
+                    </p>
+                </div>
             </div>
-            <p id="login" class="login-btn"><input type="button" value="SIGN IN" class="button" onclick="return validateLogin()"></p>
+            <div class="d-grid">
+                <p id="save" class="save-btn-register"><input type="reset" value="SIGN IN" class="button" onclick="return validateLogin()"></p>
+            </div>
             <p class="register-btn"><b>Don't have an account?<a href="register.html"> Sign Up </a><b></p>
-        </form>
+            </form>
+        </div>
     </div>
 </div>
 
