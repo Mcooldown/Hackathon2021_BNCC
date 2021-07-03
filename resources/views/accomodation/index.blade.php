cb847ac0e394aa4e91a4bc92505e3bf59d4fdd94
Mcooldown <vincenthadinata30@gmail.com>
2021-07-03 12:45:25 +0700

    add city table

--- resources/views/accomodation/index.blade.php
+++ resources/views/accomodation/index.blade.php
@@ -1,6 +1,10 @@
 @extends('layouts.app')
 
 @section('content')
+    <div class="container py-5 my-5">
+
+    </div>
+
     <table>
         @foreach ($accomodations as $accomodation)
             <tr>
