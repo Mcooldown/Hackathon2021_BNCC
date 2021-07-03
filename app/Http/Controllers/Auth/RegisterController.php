6d7a863f34aaa7c5c3031d243139f9f155d0b420
Mcooldown <vincenthadinata30@gmail.com>
2021-07-03 10:37:30 +0700

    CHANGE REGISTER

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -67,7 +67,10 @@ class RegisterController extends Controller
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
+            'role' => 'USER',
             'password' => Hash::make($data['password']),
+            'phone_number' => $data['phone_number'],
+            'balance' => 0,
         ]);
     }
 }
