82fcb8828a014e106c329338f5970eaa937ecaaf
Brian Karnadi Japar <briankarnadi43@gmail.com>
2021-07-03 21:27:44 +0700

    rating controller Auth

--- app/Http/Controllers/RatingController.php
+++ app/Http/Controllers/RatingController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Rating;
-use Auth;
+use Illuminate\Support\Facades\Auth;
 
 class RatingController extends Controller
 {
