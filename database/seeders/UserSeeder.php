b9735f86f12e53eea4a18a5f7191c69f62a04133
Mcooldown <vincenthadinata30@gmail.com>
2021-07-03 11:04:48 +0700

    add seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -17,18 +17,26 @@ class UserSeeder extends Seeder
                 'ota_name' => 'Antavaya',
                 'email' => 'antavaya@gmail.com',
                 'password' => Hash::make('abcd'),
+                'phone_number' => '123894728934',
+                'balance' => 0,
             ],
             [
                 'role' => 'ADMIN',
                 'name' => 'Bambang',
+                'ota_name' => NULL,
                 'email' => 'bambang@gmail.com',
                 'password' => Hash::make('abcd'),
+                'phone_number' => '123894728123',
+                'balance' => 0,
             ],
             [
                 'role' => 'USER',
                 'name' => 'Brian',
+                'ota_name' => NULL,
                 'email' => 'brian@gmail.com',
                 'password' => Hash::make('abcd'),
+                'phone_number' => '1232222224',
+                'balance' => 0,
             ]
         ]);
     }
