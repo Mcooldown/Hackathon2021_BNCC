ff4e5f2eb4701ac17170d586dbe3e0bcce3d5d27
KevinNathanaelTaufiek <kevin.taufiek@gmail.com>
2021-07-03 10:19:57 +0700

    checkout

--- app/Models/Checkout.php
+++ app/Models/Checkout.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Checkout extends Model
 {
     use HasFactory;
+
+    protected $fillable=[
+        'booking_id','total_pay'
+    ];
+
+    public function booking(){
+        return $this->belongsTo(Booking::class,'booking_id');
+    }
+
 }
