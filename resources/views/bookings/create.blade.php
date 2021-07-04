7143ae3877537c920f6adb248544a1fd1e9e80fd
Mcooldown <vincenthadinata30@gmail.com>
2021-07-04 10:33:05 +0700

    benerin book & cheout

--- resources/views/bookings/create.blade.php
+++ resources/views/bookings/create.blade.php
@@ -4,44 +4,45 @@
 @section('content')
     <div class="container py-5 mb-5">
         <a class="btn btn-hijau btn-lg rounded-pill"
-            href="{{ route('rooms.index', ['qty' => $qty, 'accomodation_id' => $room->accomodation->id, 'check_in' => $check_in, 'check_out' => $check_out]) }}">Back
-            to accomodations</a>
+            href="{{ route('rooms.index', ['qty' => $qty, 'accomodation_id' => $room->accomodation->id, 'check_in' => strtotime($check_in), 'check_out' => strtotime($check_out)]) }}">Back
+            to rooms</a>
         <form action="{{ route('bookings.store') }}" method="POST">
             @csrf
             <input type="hidden" name="room_id" value="{{ $_GET['room_id'] }}">
-            <div class="card my-2">
-                <div class="card-body">
-                    <div class="row">
+            <input type="hidden" name="check_in" value="{{ $_GET['check_in'] }}">
+            <input type="hidden" name="check_out" value="{{ $_GET['check_out'] }}">
+
+            <div class="card border-0 shadow rounded-30 mt-3">
+                <div class="card-body my-3">
+                    <div class="row align-items-center">
                         <div class="col-md-3">
                             <img class="rounded" src="/storage/images/{{ $room->photo }}" width="100%" alt="">
                         </div>
                         <div class="col-md-9">
-                            <h3>{{ $room->type }} - {{ $room->accomodation->name }}</h3>
-                            <p>{{ $room->accomodation->address }} | {{ $room->accomodation->city->name }},
-                                {{ $room->accomodation->city->country }}</p>
-                            <hr>
-                            <p><b>Check-in:</b>
-                                <input type="hidden" class="border-0" readonly value="{{ $_GET['check_in'] }}"
-                                    name="check_in">
-                                {{ $_GET['check_in'] }}
+                            <h3 class="fw-bold text-teal">{{ $room->type }} - {{ $room->accomodation->name }}</h3>
+                            <p class="text-muted mb-1">{{ $room->accomodation->address }} |
+                                {{ $room->accomodation->city->name }},
+                                {{ $room->accomodation->city->country }} <br>
+                                {{ $room->description }}</p>
+                            <p>Check-in: {{ date('d F Y', strtotime($_GET['check_in'])) }} |
+                                Check-out: {{ date('d F Y', strtotime($_GET['check_out'])) }}</p>
+                        </div>
+                    </div>
+                    <hr>
+                    <div class="row align-items-center">
+                        <div class="col-md-10 my-2">
+                            <p>Reserved Room Quantity: <input type="text" class="border-0" readonly
+                                    value="{{ $_GET['qty'] }}" name="quantity">
+                                <br>Price per Night: Rp{{ number_format($room->price) }}
+                                <br>Health Protocol Fee: Rp{{ number_format($room->accomodation->health_protocol_fee) }}
+                                per room
                             </p>
-                            <p> <b>Check-out:</b>
-                                <input type="hidden" class="border-0" readonly value="{{ $_GET['check_out'] }}"
-                                    name="check_out">
-                                {{ $_GET['check_out'] }}
-                            </p>
-                            <p><b>Description</b><br>{{ $room->description }}</p>
-                            <p>Room Quantity: <input type="text" class="border-0" readonly value="{{ $_GET['qty'] }}"
-                                    name="quantity"></p>
-                            <h5><b>Price per Night:</b> Rp{{ $room->price }}</h5>
-
+                        </div>
+                        <div class="col-md-2 my-2 text-center">
+                            <button type="submit" class="btn btn-hijau btn-lg rounded-pill px-4 mt-3">Book Now</button>
                         </div>
                     </div>
                 </div>
-            </div>
-            <div class="d-flex justify-content-end">
-
-                <button type="submit" class="btn btn-hijau">CREATE BOOKING</button>
             </div>
         </form>
     </div>
